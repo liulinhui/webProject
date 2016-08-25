fd736dcd91262a664c1db3f09f8989ebd52b44c1
liulinhui <1249055292@qq.com>
2016-08-25 23:14:57 +0800

    java

---
+++ java/src/main/java/com/learn/classLoadTest/BootStrapTest.java
@@ -0,0 +1,17 @@
+package com.learn.classLoadTest;
+
+
+import javax.print.DocFlavor;
+import java.net.URL;
+
+/**
+ * Created by liulinhui on 16-8-25.
+ */
+public class BootStrapTest {
+    public static void main(String[] args) {
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for (int i = 0; i < urls.length; i++) {
+            System.out.println(urls[i]);
+        }
+    }
+}
