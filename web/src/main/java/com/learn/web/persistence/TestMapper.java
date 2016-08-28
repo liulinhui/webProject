c532e8df92dc2a5f18cd2d11d4ffdbfcf71c6f6a
Your Name <http://liulh@121.42.173.129:9999/r/demo.git>
2016-08-29 05:22:22 +0800

    mybatis配置

---
+++ web/src/main/java/com/learn/web/persistence/TestMapper.java
@@ -0,0 +1,15 @@
+package com.learn.web.persistence;
+
+import com.learn.bean.test.user;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by linkage on 2016/8/29.
+ */
+@Component
+public interface TestMapper {
+    List<user> getUser();
+    List<user> getUsername(String name);
+}
--- web/src/main/java/com/learn/web/test/test.java
+++ web/src/main/java/com/learn/web/test/test.java
@@ -3,6 +3,9 @@
  */
 package com.learn.web.test;
 
+import com.learn.bean.test.user;
+import com.learn.web.persistence.TestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,16 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/test")
 public class test{
     @Value("${test}")
     private String test;
+    @Autowired
+    private TestMapper testMapper;
 
     @RequestMapping(value = "/index.html",method = {RequestMethod.GET})
     public String index(ModelMap model, HttpServletRequest request) throws Exception{
         model.put("webTest",test);
+        List<user> user=testMapper.getUser();
+        System.out.println(testMapper.getUser().size());
+        System.out.println(testMapper.getUsername("123").size());
         return "test.ftl";
     }
 
