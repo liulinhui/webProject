205ccedd368acaa5854e1f90c359e454c9d1447d
liulinhui <1249055292@qq.com>
2016-08-29 22:53:07 +0800

    web service

---
+++ web/src/main/java/com/learn/web/test/webService/Greeting.java
@@ -0,0 +1,22 @@
+package com.learn.web.test.webService;
+
+/**
+ * Created by liulinhui on 16-8-29.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
