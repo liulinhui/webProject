b64cdd7dc226405fab315c3dfc6951485ed71687
Your Name <http://liulh@121.42.173.129:9999/r/demo.git>
2016-11-08 15:45:46 +0800

    忽略

--- web/src/main/java/com/learn/web/test/webService/Quote.java
+++ web/src/main/java/com/learn/web/test/webService/Quote.java
@@ -1,39 +1,39 @@
-package com.learn.web.test.webService;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-/**
- * Created by liulinhui on 16-8-29.
- */
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Quote {
-    private String type;
-    private Value value;
-
-    public Quote() {
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Value getValue() {
-        return value;
-    }
-
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "Quote{" +
-                "type='" + type + '\'' +
-                ", value=" + value +
-                '}';
-    }
-}
+//package com.learn.web.test.webService;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+///**
+// * Created by liulinhui on 16-8-29.
+// */
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class Quote {
+//    private String type;
+//    private Value value;
+//
+//    public Quote() {
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    public Value getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(Value value) {
+//        this.value = value;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Quote{" +
+//                "type='" + type + '\'' +
+//                ", value=" + value +
+//                '}';
+//    }
+//}
--- web/src/main/java/com/learn/web/test/webService/Value.java
+++ web/src/main/java/com/learn/web/test/webService/Value.java
@@ -1,39 +1,39 @@
-package com.learn.web.test.webService;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-/**
- * Created by liulinhui on 16-8-29.
- */
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Value {
-    private Long id;
-    private String quote;
-
-    public Value() {
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getQuote() {
-        return this.quote;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setQuote(String quote) {
-        this.quote = quote;
-    }
-
-    @Override
-    public String toString() {
-        return "Value{" +
-                "id=" + id +
-                ", quote='" + quote + '\'' +
-                '}';
-    }
-}
+//package com.learn.web.test.webService;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+///**
+// * Created by liulinhui on 16-8-29.
+// */
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class Value {
+//    private Long id;
+//    private String quote;
+//
+//    public Value() {
+//    }
+//
+//    public Long getId() {
+//        return this.id;
+//    }
+//
+//    public String getQuote() {
+//        return this.quote;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setQuote(String quote) {
+//        this.quote = quote;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Value{" +
+//                "id=" + id +
+//                ", quote='" + quote + '\'' +
+//                '}';
+//    }
+//}
