039c6db3a9db45798ea24905583a6803fa44dcbc
Your Name <http://liulh@121.42.173.129:9999/r/demo.git>
2016-12-13 22:11:56 +0800

    servlet学习

---
+++ servletTest/src/main/java/com/learn/servletTest/Info.java
@@ -0,0 +1,7 @@
+package com.learn.servletTest;
+
+/**
+ * Created by linkage on 2016-12-13.
+ */
+public class Info {
+}
---
+++ servletTest/src/main/java/com/learn/servletTest/servletCycle/ServletTest1.java
@@ -0,0 +1,66 @@
+package com.learn.servletTest.servletCycle;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by linkage on 2016-12-13.
+ */
+public class ServletTest1 extends HttpServlet {
+
+    private static Logger logger = Logger.getLogger(HttpServlet.class);
+
+    public void init(ServletConfig config) {
+        logger.info("初始化开始");
+        logger.info("ServletContext==========" + config.getServletContext() + "   " +
+                "ServletName====" + config.getServletName());
+        logger.info("初始化入参name======" + config.getInitParameter("name"));
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        logger.info("请求方法==" + request.getMethod());
+        super.service(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the POST method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("访问方法为GET");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the GET method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
+}
