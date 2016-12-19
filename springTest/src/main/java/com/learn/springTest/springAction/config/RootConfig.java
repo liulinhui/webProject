65de5d6e045adf0bd1ae7732a68410588ad40d5b
liulinhui <1249055292@qq.com>
2016-12-20 00:17:10 +0800

    springTest

--- springTest/src/main/java/com/learn/springTest/springAction/config/RootConfig.java
+++ springTest/src/main/java/com/learn/springTest/springAction/config/RootConfig.java
@@ -1,4 +1,4 @@
-package com.learn.springTest.springAction;
+package com.learn.springTest.springAction.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- springTest/src/main/java/com/learn/springTest/springAction/config/WebConfig.java
+++ springTest/src/main/java/com/learn/springTest/springAction/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.learn.springTest.springAction;
+package com.learn.springTest.springAction.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
