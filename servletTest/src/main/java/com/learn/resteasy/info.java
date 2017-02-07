70f514c76b9bd3b2e668eae8e72b0f016c71a2aa
liulinhui <1249055292@qq.com>
2017-02-07 21:56:23 +0800

    maven依赖修改

---
+++ servletTest/src/main/java/com/learn/resteasy/info.java
@@ -0,0 +1,10 @@
+package com.learn.resteasy;
+
+/**
+ * Created by linkage on 2017/2/7.
+ */
+public class info {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
